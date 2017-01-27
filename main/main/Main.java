53677fc6e89aacc80f591df4fcd6f445b4f3f309
Ryan <rvincent@ycp.edu>
2017-01-27 11:20:25 -0500

    filled out the main

--- main/main/Main.java
+++ main/main/Main.java
@@ -7,7 +7,7 @@ import org.eclipse.jetty.util.thread.QueuedThreadPool;
 import org.eclipse.jetty.webapp.WebAppContext;
 
 public class Main {
-		public static main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception {
 		Server server = new Server(8081);
 
 		// Create and register a webapp context
