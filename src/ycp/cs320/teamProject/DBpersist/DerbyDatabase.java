dcf20e64fad8bd9da58fc94d94fd75d3e3848d4f
Ryan <rvincent@ycp.edu>
2017-01-30 14:34:03 -0500

    tocuhed up the initial data class and started work on derby database
    Oh boy....

--- src/ycp/cs320/teamProject/DBpersist/DerbyDatabase.java
+++ src/ycp/cs320/teamProject/DBpersist/DerbyDatabase.java
@@ -1,5 +1,102 @@
 package ycp.cs320.teamProject.DBpersist;
 
-public class DerbyDatabase {
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
+public class DerbyDatabase {
+	static {
+		try {
+			Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+		} catch (Exception e) {
+			throw new IllegalStateException("Could not load Derby driver");
+		}
+	}
+	
+	private interface Transaction<ResultType> {
+		public ResultType execute(Connection conn) throws SQLException;
+	}
+
+	private static final int MAX_ATTEMPTS = 100;
+	
+	
+	public<ResultType> ResultType executeTransaction(Transaction<ResultType> txn) {
+		try {
+			return doExecuteTransaction(txn);
+		} catch (SQLException e) {
+			throw new PersistenceException("Transaction failed", e);
+		}
+	}
+
+	public<ResultType> ResultType doExecuteTransaction(Transaction<ResultType> txn) throws SQLException {
+		Connection conn = connect();
+
+		try {
+			int numAttempts = 0;
+			boolean success = false;
+			ResultType result = null;
+
+			while (!success && numAttempts < MAX_ATTEMPTS) {
+				try {
+					result = txn.execute(conn);
+					conn.commit();
+					success = true;
+				} catch (SQLException e) {
+					if (e.getSQLState() != null && e.getSQLState().equals("41000")) {
+						// Deadlock: retry (unless max retry count has been reached)
+						numAttempts++;
+					} else {
+						// Some other kind of SQLException
+						throw e;
+					}
+				}
+			}
+
+			if (!success) {
+				throw new SQLException("Transaction failed (too many retries)");
+			}
+
+			// Success!
+			return result;
+		} finally {
+			DBUtil.closeQuietly(conn);
+		}
+	}
+
+	private Connection connect() throws SQLException {
+		Connection conn = DriverManager.getConnection("jdbc:derby:H:/workspace.newDBarea;create=true");
+
+		// Set autocommit to false to allow multiple the execution of
+		// multiple queries/statements as part of the same transaction.
+		conn.setAutoCommit(false);
+
+		return conn;
+	}
+	
+	public void createTables() {
+		
+	}
+	
+	public void loadInitialData() {
+		
+	}
+	
+	
+	// The main method creates the database tables and loads the initial data.
+		public static void main(String[] args) throws IOException {
+			System.out.println("Creating tables...");
+			DerbyDatabase db = new DerbyDatabase();
+			db.createTables();
+			System.out.println("Loading initial data...");
+			db.loadInitialData();
+			System.out.println("loaded intial data");
+			System.out.println("austin got it the first time, you never will");
+			System.out.println("you got it this time, dont get cocky");
+		}
 }
--- src/ycp/cs320/teamProject/DBpersist/InitialData.java
+++ src/ycp/cs320/teamProject/DBpersist/InitialData.java
@@ -1,5 +1,44 @@
 package ycp.cs320.teamProject.DBpersist;
 
-public class InitialData {
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
+import ycp.cs320.teamProject.DBpersist.ReadCSV;
+import ycp.cs320.teamProject.model.*;
+
+public class InitialData {
+	//user db will be for authentication and hold a primary key for users and admins
+		public static List<User> getUsers() throws IOException {
+			List<User> UserList = new ArrayList<User>();
+			ReadCSV readUser = new ReadCSV("users.csv");
+			try {
+				// auto-generated primary key for table User
+				Integer UserId = 1;
+				while (true) {
+					List<String> tuple = readUser.next();
+					if (tuple == null) {
+						break;
+					}
+					Iterator<String> i = tuple.iterator();
+					User User = new User();
+
+					// read User ID from CSV file, but don't use it
+					// auto-generate User ID, instead
+					User.setUserID(UserId++);				
+					User.setUsername(i.next());
+					User.setPassword(i.next());
+					User.setEmailAddress(i.next());
+					User.setAccountType(i.next());
+					User.setFirstName(i.next());
+					User.setLastName(i.next());
+					UserList.add(User);
+				}
+				System.out.println("UserList loaded from CSV file");			
+				return UserList;
+			} finally {
+				readUser.close();
+			}
+		}
 }
