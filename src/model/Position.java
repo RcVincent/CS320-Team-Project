0025f23b5fece856623f59577f4f6f82f9a85e12
Ryan <rvincent@ycp.edu>
2017-01-30 11:24:09 -0500

    added the position class

---
+++ src/model/Position.java
@@ -0,0 +1,24 @@
+package model;
+
+public class Position {
+	private String PositionName; 
+	
+	//find a key for finding the required level of training
+	
+	
+	public Position() {
+		
+	}
+
+	
+	
+	
+	public String getPositionName() {
+		return PositionName;
+	}
+
+	public void setPositionName(String positionName) {
+		PositionName = positionName;
+	}
+	
+}
