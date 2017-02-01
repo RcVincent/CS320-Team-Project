c92d1860066182ba179a5db86d7a06e4f32f9910
Ryan <rvincent@ycp.edu>
2017-02-01 11:54:49 -0500

    started filling out parts of the project controller

--- src/ycp/cs320/teamProject/controllers/Projectcontroller.java
+++ src/ycp/cs320/teamProject/controllers/Projectcontroller.java
@@ -8,11 +8,55 @@ import ycp.cs320.teamProject.model.*;
 
 
 public class Projectcontroller {
-	public void matchUserNameWithPassword() {
+	private IDatabase db;
+	
+	public Projectcontroller() {
+		DatabaseProvider.setInstance(new DerbyDatabase());
+		db = DatabaseProvider.getInstance();
+	}
+	
+	//User Methods and db methods 
+	public ArrayList<User> matchUserNameWithPassword(String username) {
+		
+		List<User> userList = db.matchUsernameWithPassword(username);
+		
+		ArrayList<User> users = new ArrayList<User>();
+		
+		for(User user : userList) {
+			users.add(user);
+		}
+		
+		return users;
+	}
+	
+	public ArrayList<User> getAccountInformation(String username){
+		List<User> userList = db.getAccountInfo(username);
+		
+		ArrayList<User> users = new ArrayList<User>();
+		
+		for(User user : userList) {
+			users.add(user);
+		}
+		
+		return users;
+	}
+	
+	
+	public void changeUserName(String Username, String newUsername, String password) {
 		
 	}
 	
-	public void getAccountInformation() {
+	public void addUserToDatabase(String userName, String passWord, String email, String type, String firstName, String lastname) {
+		db.addUserToDatabase(userName, passWord, email, type, firstName, lastname);
+	}
+	
+	public void DeleteUserFromDatabase(String Username, String password){
+		db.DeleteUserFromDatabase(Username, password);
+	}
+	
+	
+	//utility methods 
+	public void pushToPriorityQueue(){
 		
 	}
 }
