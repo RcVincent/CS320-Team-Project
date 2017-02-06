7a8a38f6ed08b25cfe6d472c25452379ab5206db
Ryan <rvincent@ycp.edu>
2017-02-06 11:52:10 -0500

    started the controller tests

--- junit/controller/ControllerTests.java
+++ junit/controller/ControllerTests.java
@@ -1,5 +1,15 @@
 package controller;
+import static org.junit.Assert.*;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import ycp.cs320.teamProject.controllers.Projectcontroller;;
 public class ControllerTests {
-
+	private Projectcontroller controller;
+	
+	 @Before
+	 public void setUp() {
+		 
+	 }
 }
