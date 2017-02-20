093ddbbdb25b7e390924e3bd2f25a5d7bcd961d9
Ryan <rvincent@ycp.edu>
2017-02-20 10:31:18 -0500

    filling out the position class

--- src/ycp/cs320/teamProject/model/Position.java
+++ src/ycp/cs320/teamProject/model/Position.java
@@ -1,17 +1,19 @@
 package ycp.cs320.teamProject.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Position {
 	private String PositionName; 
 	
 	//find a key for finding the required level of training
-	
+	private int positionID;
+	private List<Integer> regulatingSOPs = new ArrayList<Integer>();
 	
 	public Position() {
 		
 	}
 
-	
-	
 	
 	public String getPositionName() {
 		return PositionName;
@@ -19,6 +21,37 @@ public class Position {
 
 	public void setPositionName(String positionName) {
 		PositionName = positionName;
+	}
+
+	public int getPositionID() {
+		return positionID;
+	}
+
+	public void setPositionID(int positionID) {
+		this.positionID = positionID;
+	}
+
+	public List<Integer> getRegulatingSOPs() {
+		return regulatingSOPs;
+	}
+
+	public void setRegulatingSOPs(List<Integer> regulatingSOPs) {
+		this.regulatingSOPs = regulatingSOPs;
+	}
+	
+	public void addRegulatedSOP(int SOPID) {
+		regulatingSOPs.add(SOPID);
+	}
+	
+	public int findRelevantSOP(int SOPID) {
+		int referenceID = 0; 
+		for(int i = 0; i < regulatingSOPs.size(); i++) {
+			if(regulatingSOPs.get(i).equals(SOPID)) {
+				referenceID = regulatingSOPs.get(i);
+			}
+		}
+		
+		return referenceID;
 	}
 	
 }
