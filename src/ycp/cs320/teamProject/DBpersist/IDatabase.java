5fbb09d3f2f2d3ce1537a415359c5f241ec1766f
Ryan <rvincent@ycp.edu>
2017-03-10 11:24:32 -0500

    just added a few comments to guide later designs

--- src/ycp/cs320/teamProject/DBpersist/IDatabase.java
+++ src/ycp/cs320/teamProject/DBpersist/IDatabase.java
@@ -14,16 +14,26 @@ public interface IDatabase {
 	 List<User> addUserToDatabase(String name, String pswd, String email, String type, String first, String last);
 	 List<User> DeleteUserFromDatabase(String name, String pswd);
 	 List<User> changeUsername(final String name, final String newName, final String pswd);
+	 //possibly change 'changeUsername' to 'changePassword'
+	 //rework 'delete users from database' to archive 'usersInDatabase'
 	 
 	 
 	 //*******************
 	 //SOP related methods implemented in derby database
 	 //*******************
 	 
+	 //arhive sop
+	 
 	 //*******************
-	 //Training related methods implemented in derby database
+	 //Training History related methods implemented in derby database
 	 //*******************
 	 
+	 	//going to have a search in this section
+	 	//implement sorts
+	 
+	 //******************
+	 //Utility Based methods
+	 //******************
 	 
 	
 }
