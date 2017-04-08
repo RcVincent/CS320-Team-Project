35fd47f1d5c35eb8b6883312240fb9cfdbf76d8e
DLHartman <DLHartman83@yahoo.com>
2017-04-08 13:35:55 -0400

    Added Positions to DB, still needs to be trouble shot.

--- src/ycp/cs320/teamProject/model/Position.java
+++ src/ycp/cs320/teamProject/model/Position.java
@@ -4,32 +4,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Position {
+
+	private int positionID;
+	private String positionName; 
+	private String positionDuty;
 	
-	private String PositionName = "Lord Commander"; 
-	private int positionID = 001;
+	// is this needed with the way we are linking them in the DB
 	private List<SOP> regulatingSOPs = new ArrayList<SOP>();
-	
+
 	//find a key for finding the required level of training
-	
+
 	public Position() {
-		
+
 	}
 
-	
+
 	public String getPositionName() {
-		return PositionName;
+		return positionName;
 	}
 
-	public void setPositionName(String positionName) {
-		PositionName = positionName;
+	public void setPositionName(String PositionName) {
+		positionName = PositionName;
 	}
 
 	public int getPositionID() {
 		return positionID;
 	}
 
-	public void setPositionID(int positionID) {
-		this.positionID = positionID;
+	public void setPositionID(int PositionID) {
+		this.positionID = PositionID;
 	}
 
 	public List<SOP> getRegulatingSOPs() {
@@ -39,11 +42,11 @@ public class Position {
 	public void setRegulatingSOPs(List<SOP> regulatingSOPs) {
 		this.regulatingSOPs = regulatingSOPs;
 	}
-	
+
 	public void addRegulatedSOP(SOP sop) {
 		regulatingSOPs.add(sop);
 	}
-	
+
 	public int findRelevantSOP(int sopID) {
 		int referenceID = 0; 
 		for(int i = 0; i < regulatingSOPs.size(); i++) {
@@ -52,19 +55,29 @@ public class Position {
 				break;
 			}
 		}
-		
+
 		return referenceID;
 	}
-	
+
 	public ArrayList<SOP> findRelevantSOPs(Position position) {
 		ArrayList<SOP> sops = new ArrayList<SOP>();
-		
+
 		for(int i = 0; i < regulatingSOPs.size(); i++) {
 			if(position.getPositionID() == regulatingSOPs.get(i).getPositionsAffected().get(i).getPositionID()) {
 				sops.add(regulatingSOPs.get(i));
 			}
 		}
-		
+
 		return sops;
+	}
+
+
+	public String getPositionDuty() {
+		return positionDuty;
+	}
+
+
+	public void setPositionDuty(String PositionDuty) {
+		positionDuty = PositionDuty;
 	}
 }
