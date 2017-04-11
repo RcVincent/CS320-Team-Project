c69fbd56ca3be0e09558c4c9b3519438254e3b13
Ryan <rvincent@ycp.edu>
2017-04-11 16:22:14 -0400

    removed junction class, added the sop-position class and the
    user-position class

---
+++ src/ycp/cs320/teamProject/model/PositionSOP.java
@@ -0,0 +1,27 @@
+package ycp.cs320.teamProject.model;
+
+public class PositionSOP {
+	private int sopID, PositionID;
+	
+	public PositionSOP() {
+		
+	}
+
+	public int getSopID() {
+		return sopID;
+	}
+
+	public void setSopID(int sopID) {
+		this.sopID = sopID;
+	}
+
+	public int getPositionID() {
+		return PositionID;
+	}
+
+	public void setPositionID(int positionID) {
+		PositionID = positionID;
+	}
+	
+	
+}
---
+++ src/ycp/cs320/teamProject/model/UserPosition.java
@@ -0,0 +1,27 @@
+package ycp.cs320.teamProject.model;
+
+public class UserPosition {
+	private int PositionID, UserID;
+	
+	public UserPosition() {
+		
+	}
+
+	public int getPositionID() {
+		return PositionID;
+	}
+
+	public void setPositionID(int positionID) {
+		PositionID = positionID;
+	}
+
+	public int getUserID() {
+		return UserID;
+	}
+
+	public void setUserID(int userID) {
+		UserID = userID;
+	}
+	
+		
+}
